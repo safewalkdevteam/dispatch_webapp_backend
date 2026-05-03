4c5abbb94470fd9bcfc3b218257fab11fb90f46b
Nevin <nevinxuregina@gmail.com>
2026-05-03 00:32:24 -0600

    initial commit

---
+++ src/main/java/com/safewalk/dispatch_webapp/DispatchWebappApplication.java
@@ -0,0 +1,13 @@
+package com.safewalk.dispatch_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safewalk/dispatch_webapp/DispatchWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safewalk.dispatch_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
